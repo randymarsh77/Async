a5f12183da5e5e8c17929a2b891a2ffd35a07c78
Matthew Preucil <mattpsbs@gmail.com>
2016-11-19 18:48:44 -0800

    Add travis.yml

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "Async",
     dependencies: [
-		.Package(url: "https://www.github.com/randymarsh77/casync", majorVersion: 1),
+		.Package(url: "https://github.com/randymarsh77/casync", majorVersion: 1),
 	]
 )
